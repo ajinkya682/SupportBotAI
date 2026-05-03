3fb64e3e485849d8f51bf64fd2c8f3fc839881f3
Ayaan Ahmad <ayaanah287@gmail.com>
2026-05-04 05:12:35 +0530

    Add new backend feature code

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
+import authRoutes from './routes/auth.routes.js';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+app.use('/api/auth', authRoutes);
 
 export default app;
